34f4e8d58d340e67ed52390c636879d7eb8bd04d
zhengxq27 <1172009120@qq.com>
2019-05-05 15:24:16 +0800

    renew project

--- photo/app/src/main/java/com/study/sysu/photo/MainActivity.java
+++ photo/app/src/main/java/com/study/sysu/photo/MainActivity.java
@@ -5,6 +5,9 @@ import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.view.ViewPager;
@@ -15,12 +18,20 @@ import com.ogaclejapan.smarttablayout.SmartTabLayout;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItemAdapter;
 import com.ogaclejapan.smarttablayout.utils.v4.FragmentPagerItems;
 
+import java.io.File;
+import java.util.ArrayList;
+
+import static com.study.sysu.photo.MyApplication.getMyApplication;
+
 public class MainActivity extends AppCompatActivity {
 
     private static final int REQUEST_EXTERNAL_STORAGE = 1;
     private static String[] PERMISSIONS_STORAGE = {
             "android.permission.READ_EXTERNAL_STORAGE",
             "android.permission.WRITE_EXTERNAL_STORAGE" };
+    private MyApplication app;
+    private ArrayList<SpacePhoto> all_photo_set = new ArrayList<>(); // 存放所有图片的路径
+    private ArrayList<album> all_album = new ArrayList<>(); //按系统相册所属分开照片
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,6 +53,11 @@ public class MainActivity extends AppCompatActivity {
 
         SmartTabLayout viewPagerTab = (SmartTabLayout) findViewById(R.id.viewpagertab);
         viewPagerTab.setViewPager(viewPager);
+
+        search_all_picture();
+        app = (MyApplication)getMyApplication();
+        app.set_all_photo_set(all_photo_set); //放到Application中
+        app.set_all_album(all_album);
     }
 
     public static void verifyStoragePermissions(Activity activity) {
@@ -59,6 +75,47 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    public void search_all_picture() { // 获取系统中所有图片的路径
+        Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
+        String[] projImage = { MediaStore.Images.Media._ID
+                , MediaStore.Images.Media.DATA
+                ,MediaStore.Images.Media.SIZE
+                ,MediaStore.Images.Media.DISPLAY_NAME};
+        Cursor mCursor = this.getContentResolver().query(mImageUri,
+                projImage,
+                MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=?",
+                new String[]{"image/jpeg", "image/png"},
+                MediaStore.Images.Media.DATE_MODIFIED+" desc");
+
+        if( mCursor != null )
+        {
+            while(mCursor.moveToNext()){
+                String path = mCursor.getString(mCursor.getColumnIndex(MediaStore.Images.Media.DATA));
+                String displayName = mCursor.getString(mCursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
+                all_photo_set.add(new SpacePhoto(path,displayName)); //将获取到的路径加入路径集合
+
+                String dirPath = new File(path).getParentFile().getAbsolutePath(); // 获取该图片的父路径名
+
+                int pdf = 0; //判断此图片所属的相册是否已存在，pdf为0表示图片所属的相册还不存在
+                for(int i = 0; i < all_album.size(); i++){
+                    if( all_album.get(i).getDirpath().equals(dirPath)){
+                        pdf = 1;
+                        all_album.get(i).add(new SpacePhoto(path,displayName));
+                    }
+                }
+                if( pdf == 0){
+                    ArrayList<SpacePhoto> new_list = new ArrayList<>();
+                    new_list.add(new SpacePhoto(path,displayName));
+                    String Str[] = dirPath.split("/");
+                    String album_name = Str[Str.length - 1];
+                    all_album.add(new album(new_list,dirPath,album_name));
+                }
+
+            }
+
+        }
+
+    }
 }
 
 
--- photo/app/src/main/java/com/study/sysu/photo/PageFragment1.java
+++ photo/app/src/main/java/com/study/sysu/photo/PageFragment1.java
@@ -24,9 +24,8 @@ import static com.study.sysu.photo.MyApplication.getMyApplication;
 
 public class PageFragment1 extends Fragment {
 
-    private ArrayList<SpacePhoto> all_photo_set = new ArrayList<>(); // 存放所有图片的路径
-    private ArrayList<album> all_album = new ArrayList<>(); //按系统相册所属分开照片
     private MyApplication app;
+    private ArrayList<SpacePhoto> all_photo_set = new ArrayList<>(); // 存放所有图片的路径
 
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, // 系统会在Fragment首次绘制其用户界面时调用此方法
@@ -34,11 +33,8 @@ public class PageFragment1 extends Fragment {
 
         View root = inflater.inflate(R.layout.fragment_page1,container,false);
 
-        search_all_picture();
         app = (MyApplication)getMyApplication();
-        app.set_all_photo_set(all_photo_set); //放到Application中
-        app.set_all_album(all_album);
-
+        all_photo_set = app.get_all_photo_set();
         RecyclerView.LayoutManager layoutManager = new GridLayoutManager(getActivity(), 2);
         RecyclerView recyclerView = (RecyclerView) root.findViewById(R.id.rv_images_1);
         recyclerView.setHasFixedSize(true);
@@ -50,47 +46,7 @@ public class PageFragment1 extends Fragment {
         return root;
     }
 
-    public void search_all_picture() { // 获取系统中所有图片的路径
-        Uri mImageUri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI;
-        String[] projImage = { MediaStore.Images.Media._ID
-                , MediaStore.Images.Media.DATA
-                ,MediaStore.Images.Media.SIZE
-                ,MediaStore.Images.Media.DISPLAY_NAME};
-        Cursor mCursor = getActivity().getContentResolver().query(mImageUri,
-                projImage,
-                MediaStore.Images.Media.MIME_TYPE + "=? or " + MediaStore.Images.Media.MIME_TYPE + "=?",
-                new String[]{"image/jpeg", "image/png"},
-                MediaStore.Images.Media.DATE_MODIFIED+" desc");
 
-        if( mCursor != null )
-        {
-            while(mCursor.moveToNext()){
-                String path = mCursor.getString(mCursor.getColumnIndex(MediaStore.Images.Media.DATA));
-                String displayName = mCursor.getString(mCursor.getColumnIndex(MediaStore.Images.Media.DISPLAY_NAME));
-                all_photo_set.add(new SpacePhoto(path,displayName)); //将获取到的路径加入路径集合
-
-                String dirPath = new File(path).getParentFile().getAbsolutePath(); // 获取该图片的父路径名
-
-                int pdf = 0; //判断此图片所属的相册是否已存在，pdf为0表示图片所属的相册还不存在
-                for(int i = 0; i < all_album.size(); i++){
-                    if( all_album.get(i).getDirpath().equals(dirPath)){
-                        pdf = 1;
-                        all_album.get(i).add(new SpacePhoto(path,displayName));
-                    }
-                }
-                if( pdf == 0){
-                    ArrayList<SpacePhoto> new_list = new ArrayList<>();
-                    new_list.add(new SpacePhoto(path,displayName));
-                    String Str[] = dirPath.split("/");
-                    String album_name = Str[Str.length - 1];
-                    all_album.add(new album(new_list,dirPath,album_name));
-                }
-
-            }
-
-        }
-
-    }
 
 
 }
